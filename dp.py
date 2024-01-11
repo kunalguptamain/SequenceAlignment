de86ea689754115b90bdb0815ad3e4e4c18c2f41
Kunal Gupta <kunalguptazool9@gmail.com>
2024-01-10 19:03:23 -0800

    global alignment

--- dp.py
+++ dp.py
@@ -1,27 +1,72 @@
+def print_2d_array(r):
+    for i in range(len(r)):
+        for j in range(len(r[i])):
+            print("%3d" % r[i][j], end="")
+        print()
+
+
 def global_alignment(sequence_1: str, sequence_2: str, gap:int, substitution_matrix):
-    game_board = [0] * len(sequence_2)
+    #setting gameboard
+    game_board = [[0] * (len(sequence_2) + 1)]
     for i in range(len(sequence_1)):
         row_i = [0]
         for j in range(len(sequence_2)):
             row_i.append(substitution_matrix[sequence_1[i]][sequence_2[j]])
         game_board.append(row_i)
 
+    #print_2d_array(game_board)
+
+    #choosing best path
     dynamic_cost_array = []
     for i in range(len(sequence_1)):
         row_i = []
         for j in range(len(sequence_2)):
             if i == 0 and j == 0: 
                 row_i.append(0)
-                break
+                continue
             if i == 0:
-                row_i.append(row_i[i][j - 1] - gap)
-                break
+                row_i.append(row_i[j - 1] - gap)
+                continue
             if j == 0:
                 row_i.append(dynamic_cost_array[i - 1][j] - gap)
-                break
-            row_i.append(max(row_i[i][j - 1] - gap, dynamic_cost_array[i - 1][j] - gap, \
+                continue
+            row_i.append(max(row_i[j - 1] - gap, dynamic_cost_array[i - 1][j] - gap, \
                              dynamic_cost_array[i - 1][j - 1] + game_board[i][j]))
-        dynamic_cost_array.appennd(row_i)
+        dynamic_cost_array.append(row_i)
+
+    #backtracking
+    row = len(sequence_1) - 1
+    col = len(sequence_2) - 1
+    aligned_sequence_1 = sequence_1[row]
+    aligned_sequence_2 = sequence_2[col]
+    while row != 0 and col != 0:
+        if row != 0 and dynamic_cost_array[row - 1][col] - dynamic_cost_array[row][col] == gap:
+            aligned_sequence_1 += sequence_1[row - 1]
+            aligned_sequence_2 += "-"
+            row -= 1
+        elif col != 0 and dynamic_cost_array[row][col - 1] - dynamic_cost_array[row][col] == gap:
+            aligned_sequence_1 += "-"
+            aligned_sequence_2 += sequence_2[ col- 1]
+            col -= 1
+        else:
+            aligned_sequence_1 += sequence_1[row - 1]
+            aligned_sequence_2 += sequence_2[col - 1]
+            row -= 1
+            col -= 1
             
+    return aligned_sequence_1[::-1], aligned_sequence_2[::-1]
+
+#    A  T  C  G
+substitution_matrix = {
+    "A" : {"A": 1, "T": -1/2, "C": -1, "G": -1},
+    "T" : {"A": -1/2, "T": 1, "C": -1, "G": -1},
+    "C" : {"A": -1, "T": -1, "C": 1, "G": -1/2},
+    "G" : {"A": -1, "T": -1, "C": -1/2, "G": 1}
+}
+
+sequences = global_alignment("CCCCCCATGCGTAGTCGTCAGCTAGCC", "ACTATCTAACGGTC", 1, substitution_matrix)
+print(sequences[0])
+print(sequences[1])
+
     
     
